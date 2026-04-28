3ffead09262502227cba4722089b80e75a9e3398
YP <ypsmacbookair@YPs-MacBook-Air.local>
2026-04-28 22:55:04 +1000

    Improved scan speed using threading

--- port_scanner.py
+++ port_scanner.py
@@ -1,21 +1,27 @@
 import socket
+import threading
+
 def scan_port(ip, port):
     sock = socket.socket()
-    sock.settimeout(1)
+    sock.settimeout(0.3)
     result = sock.connect_ex((ip, port))
     sock.close()
-
     if result == 0:
-        return True
-    else:
-        return False
-    
+        print(f"Port {port} -- OPEN")
+
 def run_scanner(ip, start_port, end_port):
     print(f"Scanning {ip}...")
-
+    threads = []
+    
     for port in range(start_port, end_port + 1):
-        if scan_port(ip, port) == True:
-            print(f"Port {port} -- OPEN")
+        t = threading.Thread(target=scan_port, args=(ip, port))
+        threads.append(t)
+        t.start()
+    
+    for t in threads:
+        t.join()
+    
+    print("Scan complete!")
 
-target = input(" Enter the IP you want to scan: ")
-run_scanner(target, 1, 100)
+target = input("Enter the IP you want to scan: ")
+run_scanner(target, 1, 1024)
