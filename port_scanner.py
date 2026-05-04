56b8e4fa54123ff3340a549679ff0e53afeda26e
YP <ypsmacbookair@YPs-MacBook-Air.local>
2026-05-04 12:20:23 +1000

    added csv export of scan results

--- port_scanner.py
+++ port_scanner.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+import csv
+from datetime import datetime
+results = []
 
 def scan_port(ip, port):
     sock = socket.socket()
@@ -7,7 +10,13 @@ def scan_port(ip, port):
     result = sock.connect_ex((ip, port))
     sock.close()
     if result == 0:
+        status = "OPEN"
         print(f"Port {port} -- OPEN")
+    else:
+        status = "CLOSED"
+    
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    results.append([timestamp,ip, port, status])
 
 def run_scanner(ip, start_port, end_port):
     print(f"Scanning {ip}...")
@@ -23,5 +32,15 @@ def run_scanner(ip, start_port, end_port):
     
     print("Scan complete!")
 
+def save_to_csv(ip):
+    filename = f"scan_{ip}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["TimeStamp", "Target IP", "Port", "Status"])
+        writer.writerows(results)
+    print(f"Results saved to {filename}")
+
 target = input("Enter the IP you want to scan: ")
 run_scanner(target, 1, 1024)
+save_to_csv(target)
